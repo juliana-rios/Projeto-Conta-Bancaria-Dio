3e8dd677e409a8e2de05f2184dce1bee23a5426b
Juliana Rios <julianariosone@gmail.com>
2023-06-04 18:58:24 -0300

    Primeiro update

---
+++ Conta-Bancaria/src/main/java/org/dio/ContaTerminal.java
@@ -0,0 +1,12 @@
+package org.dio;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        //TODO:Conhecer e importar a classe scanner
+
+
+        //Exibir as mensagens para o nosso usuario
+
+        //Exibir mensagem conta criada
+    }
+}
